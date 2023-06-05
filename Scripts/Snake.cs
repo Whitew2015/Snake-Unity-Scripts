1acbef318905279bcca5863ff6e52ee85ad071bd
Winston White Jr <whitew2015@fau.edu>
2023-06-05 13:14:18 -0400

    added movement controls

--- Scripts/Snake.cs
+++ Scripts/Snake.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 _direction = Vector2.right;
+
+    private void Update()
     {
-        
+        // control direction of the snake based on key presses
+        if (Input.GetKeyDown(KeyCode.W)) {
+            _direction = Vector2.up;
+        } else if (Input.GetKeyDown(KeyCode.S)) {
+            _direction = Vector2.down;
+        } else if (Input.GetKeyDown(KeyCode.A)) {
+            _direction = Vector2.left;
+        } else if (Input.GetKeyDown(KeyCode.D)) {
+            _direction = Vector2.right;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    //fixed time interval
+    private void FixedUpdate()
     {
-        
+        //aligns the snake on the grid 
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x) + _direction.x,
+            Mathf.Round(this.transform.position.y) + _direction.y,
+            0.0f
+        );
     }
 }
